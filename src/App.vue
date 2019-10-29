83cd5f7fb968889942223120bd708bd9e8940c71
ideago <yexuehan63@126.com>
2019-10-29 11:59:37 +0800

    $emit 和 v-on指令的配合

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Welcome to" />
     <ButtonCounter />
+    <div :style="{fontSize: postFontSize + 'em'}">
+      <BlogPost
+        v-on:enlarge-text="handleEnlarge"
+        v-for="blog in blogs"
+        v-bind:key="blog.id"
+        v-bind:title="blog.title"
+        v-bind:content="blog.content"
+      ></BlogPost>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import ButtonCounter from './components/ButtonCounter.vue'
-
+import BlogPost from './components/BlogPost.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    ButtonCounter
+    ButtonCounter,
+    BlogPost
+  },
+  data: function () {
+    return {
+      postFontSize: 1,
+      blogs: [
+        {
+          id: 1,
+          title: 'Vue的props到底怎么回事',
+          content: 'Vue的props到底怎么回事,得具体看看才行',
+        },
+        {
+          id: 2,
+          title: 'v-for指令的使用',
+          content: '纸上来得终觉浅，绝知此事要躬行',
+        },
+        {
+          id: 3,
+          title: '$emit 和v-on指令',
+          content: '很神奇哇,$emit发了一份，v-on绑定的接收handle只执行了一次',
+        }
+      ]
+    }
+  },
+  methods: {
+    created () {
+
+    },
+    mounted () {
+
+    },
+    handleEnlarge () {
+      console.log(`handleEnlarge`);
+      this.postFontSize += 0.1
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>{{title}}</h3>
+    <button v-on:click="$emit('enlarge-text')">Enlarge</button>
+    <p>{{content}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:{
+      title:String,
+      content:String,
+      id:Number
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
